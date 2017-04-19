6504dfa053a2b77dcc8de4e477ee3f4daba7e382
exampleprogrammingbugs <exampleprogrammingbugs@gmail.com>
2017-04-19 14:09:34 +0100

    Add files via upload

--- bruteforcer/Brute_Forcer_1/Brute_Forcer_1.ino
+++ bruteforcer/Brute_Forcer_1/Brute_Forcer_1.ino
@@ -1,74 +1,44 @@
-#include <Servo.h> //Header file for servo
+#include <Servo.h>
 
-/*
- * Arduino code for the Noke bruteforcer.
- * 
- * Demonstrates opening a Noke Quick Click Combination of 4
- * short presses followed by 4 long presses.
- * 
- */
-
-//Time in ms for a long press
-#define DASH 455
-//Time in ms for a short press
-#define DOT 150
-//Servo setting for raising the arm
-#define UP 95
-//Servo setting for lowering the arm
-#define DOWN 70
-//Short delay to wait between presses
-#define GAP 75
-//Servo setting for when entry is finished
-#define OPEN 180
-#define UNLOCKED
-
-int analogPin = 0;
-int val = 0;
-
+#define DASH 455        //Time in ms for a long press
+#define DOT 150         //Time in ms for a short press
+#define UP 95           //Servo setting for raising the arm
+#define DOWN 70         //Servo setting for lowering the arm
+#define GAP 75          //Short delay to wait between presses
+#define OPEN 180        //Servo setting for when entry is finished
 Servo servo; 
 
 void setup() {
-  // put your setup code here, to run once:
-servo.attach(9);
-Serial.begin(9600);
-delay(500);
+  servo.attach(9);
+  Serial.begin(9600);
+  delay(500);
 }
 
 void loop() {
-  dot();//Align servo arm
-  //Compute 4 dots followed by 4 dashes
+  dot();                //Align servo arm
+  //An example: execute 4 dots followed by 4 dashes
   for(int i = 0; i < 4; i++){
     dot();
-    }
+  }
   for(int i = 0; i < 4; i++){
     dash();
-    }
-  //Once test combination is finished, raise servo arm.
-  servo.write(OPEN);
-  //Delay before reattempting
-  delay(15000);
+  }
+  servo.write(OPEN);    //Once test combination is finished, raise servo arm
+  delay(15000);         //Delay before reattempting
 }
 
-/*
- * Dot inputs a short press
- * writes to the servo for a short press
- * Short delay once finished
- * 
-  */void dot(){
+// dot(): perform a short press write to the servo, followed by a gap
+void dot(){
   servo.write(DOWN);
   delay(DOT);
   servo.write(UP);
   delay(GAP);
-  }
+}
 
-/*
- * Dash inputs a long press
- * writes to the servo for a long press
- * Short delay once finished
- */
+// dash(): perform a long press write to the servo, followed by a gap
 void dash(){
   servo.write(DOWN);
   delay(DASH);
   servo.write(UP);
   delay(GAP);
-  }
+}
